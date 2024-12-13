e652f10315c4e62f13a742e4bd852dff69cbe047
sumit <108910264+sumit-davda@users.noreply.github.com>
2024-12-13 12:19:49 +0530

    Add files via upload

---
+++ employee_management_system/challenge/src/app.py
@@ -0,0 +1,30 @@
+
+def calculateTotalSalary(employees):
+    '''
+    Calculate and return the total salary of all employees.
+    '''
+    
+    # write your code here
+    
+def searchStudentByName(employees,empName):
+    '''
+    Search for an employee in the list based on the given name and return True if the employee exists; otherwise, return False.
+    '''
+    
+        
+def countEmployeeByDept(employees,deptName):
+    '''
+    Find the total number of employees in the given department and return the value of count.
+    '''
+    
+    
+    
+    # write your code here
+    
+    
+def updateSalary(employees):
+    '''
+    Apply a 10% increment to the salary of all employees and return the updated list of employees.
+    '''
+    
+    
